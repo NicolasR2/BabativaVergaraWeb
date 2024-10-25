462c82826a3530afc34db9de150f944ea34ece9c
TavoTksh1 <tavo.tkshi@gmail.com>
2024-10-24 23:25:24 -0500

    ddd

--- src/components/Nosotros/components/Persona1.jsx
+++ src/components/Nosotros/components/Persona1.jsx
@@ -8,10 +8,10 @@ function Persona1() {
   return (
     <div className="fu-container">
       <div className='fu-text'>
-      <h1 className='fu-title'> Manuel Esteban Ruiz </h1>
+      <h1 className='fu-title'> Luis Antoncio Babativa Vergara </h1>
       <a className='fu-street'> Especialista en derecho judicial</a>
-      <a className='fu-build'> Mas de 5 años de experiencia</a>
-      <a className='fu-office'> Manuel@gmail.com</a>
+      <a className='fu-build'> Mas de 15 años de experiencia</a>
+      <a className='fu-office'> BabativayVergara@gmail.com</a>
       <a className='fu-country'><em>Bogotá D.C. Colombia</em></a>
       </div>
       <div className='fu-map'>
